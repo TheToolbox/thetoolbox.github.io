700c91cc7ef3afc6dd79d78c94a27d15eb58c6df
Toolbox <jpo3@duke.edu>
2015-11-07 21:40:59 -0500

    sdfhjisdg

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-/*function initialize() {
-	var mapCanvas = document.getElementById('map');
+
+
+
+//LINK: https://gitlab.oit.duke.edu/toolbox/hackduke15
+
+document.addEventListener("DOMContentLoaded", function () {
+
+	var mapCanvas = document.querySelector('#map');
 	var mapOptions = {
 		center: new google.maps.LatLng(44.5403, -78.5463),
 		zoom: 8,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(mapCanvas, mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-*/
 
-//LINK: https://gitlab.oit.duke.edu/toolbox/hackduke15
 
-document.addEventListener("DOMContentLoaded", function () {
+
+
+
+
+
+
+
+
 
 	document.querySelector("#starter").addEventListener("click", startTimer);
 
