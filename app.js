19e21d4fdcb28096a352c819684a4dec6070a341
Toolbox <jpo3@duke.edu>
2015-11-07 19:51:47 -0500

    cahnged

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function initialize() {
+/*function initialize() {
 	var mapCanvas = document.getElementById('map');
 	var mapOptions = {
 		center: new google.maps.LatLng(44.5403, -78.5463),
@@ -8,3 +8,20 @@ function initialize() {
 	var map = new google.maps.Map(mapCanvas, mapOptions);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+	
+	document.querySelector("#starter").addEventListener("click",startTimer);
+	
+	var timer;
+	function startTimer() {
+		timer = setTimeout(timeIsUp,10000);
+	}
+	
+	function timeIsUp() {
+		alert("BAD THINGS");
+	}
+});
+
+
