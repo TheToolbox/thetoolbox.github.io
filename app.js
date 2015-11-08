0578e23bffbac402a7f37744e72883c653a947cb
Toolbox <jpo3@duke.edu>
2015-11-07 21:16:06 -0500

    stuff

--- app.js
+++ app.js
@@ -10,18 +10,58 @@
 google.maps.event.addDomListener(window, 'load', initialize);
 */
 
-document.addEventListener("DOMContentLoaded", function() {
-	
-	document.querySelector("#starter").addEventListener("click",startTimer);
-	
+//LINK: https://gitlab.oit.duke.edu/toolbox/hackduke15
+
+document.addEventListener("DOMContentLoaded", function () {
+
+	document.querySelector("#starter").addEventListener("click", startTimer);
+
 	var timer;
+	var interval;
 	function startTimer() {
 		timer = Date.now();
-		setInterval(updateTime,10000);
+		interval = setInterval(updateTime, 100);
+		document.querySelector("#starter").innerHTML = "Cancel Trouble Timer";
+		document.querySelector("#starter").removeEventListener("click", startTimer);
+		document.querySelector("#starter").addEventListener("click", stopTimer);
 	}
-	
+
+	function stopTimer() {
+		clearInterval(interval);
+		document.querySelector("#countdown").innerHTML = "10 seconds";
+		document.querySelector("#starter").innerHTML = "Start Trouble Timer";
+		document.querySelector("#starter").removeEventListener("click", stopTimer);
+		document.querySelector("#starter").addEventListener("click", startTimer);
+	}
+
 	function updateTime() {
-		alert("BAD THINGS");
+		var left = 10 - (Date.now() - timer) / 1000;
+		left = Math.round(10 * left) / 10;
+		if (left < 0) {
+			document.querySelector("#countdown").innerHTML = "0 seconds";
+			timeIsUp();
+		} else {
+			document.querySelector("#countdown").innerHTML = left + " seconds";
+		}
+
+	}
+
+	var watchID = navigator.geolocation.watchPosition(function (position) {
+		setPosition(position.coords.latitude, position.coords.longitude);
+	});
+	
+	function setPosition(lat, lon) {
+		document.querySelector("#coordinates").innerHTML = lat+", "+lon;
+	}
+
+	function sendMessage() {
+		// we need to send a message here:
+		//https://[sid]:[authtoken]@api.twilio.io/2010-04-01/Accounts/[acountsid]/Messages
+		
+	}
+
+	function timeIsUp() {
+		clearInterval(interval);
 	}
 });
 
